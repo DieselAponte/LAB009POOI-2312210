e2a4fba91c62d6b923e37ebd5851709d3a4c3790
JesusCmg <142682642+JesusCmgCode@users.noreply.github.com>
2023-10-30 23:34:26 -0500

    Jesus Camargo

--- EstudianteSSSS/src/estudiantessss/EjercicioCalificado.java
+++ EstudianteSSSS/src/estudiantessss/EjercicioCalificado.java
@@ -1,9 +1,93 @@
 package EstudianteSSSS;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class EjercicioCalificado {
+
     public static void main(String[] args) {
         Estudiantes2023 listaEstudiantes = new Estudiantes2023();
-        System.out.println("Total de estudiantes cargados: " + listaEstudiantes.tamano());
-        listaEstudiantes.listarEstudiantes();
-    }
-}
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
+
+        System.out.println("****** BIENVENIDOS AL SISTEMA DE GESTION DE ESTUDIANTES ******");
+
+        do {
+            System.out.println("1-NUEVO ESTUDIANTE");
+            System.out.println("2-BUSCAR ESTUDIANTE");
+            System.out.println("3-ELIMINAR ESTUDIANTE");
+            System.out.println("4-MODIFICAR ESTUDIANTE");
+            System.out.println("5-MODIFICAR APELLIDOS EN MINÚSCULAS");
+            System.out.println("6-VER TODOS LOS ESTUDIANTES");
+            System.out.println("7-VER TODOS LOS ESTUDIANTES POR APELLIDOS");
+            System.out.println("8-VER TODOS LOS ESTUDIANTES POR PENSIÓN");
+            System.out.println("9-TOTAL DE PENSIONES");
+            System.out.println("10-SALIR");
+            System.out.print("¿QUÉ ACTIVIDAD DESEA EJECUTAR? ");
+            opcion = scanner.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("****** REGISTRAR A UN NUEVO ESTUDIANTE ******");
+                    System.out.print("INGRESE NOMBRES: ");
+                    String nombres = scanner.next();
+                    System.out.print("INGRESE APELLIDOS: ");
+                    String apellidos = scanner.next();
+                    System.out.print("INGRESE CICLO: ");
+                    int ciclo = scanner.nextInt();
+                    System.out.print("INGRESE PENSION: ");
+                    double pension = scanner.nextDouble();
+
+                    // Crea un nuevo estudiante y agrégalo a la lista de estudiantes
+                    Estudiante nuevoEstudiante = new Estudiante(listaEstudiantes.tamano() + 1, nombres + " " + apellidos, ciclo, pension);
+                    listaEstudiantes.adicionar(nuevoEstudiante);
+                    System.out.println("¡Se añadió con éxito!");
+                    break;
+                case 2:
+                    System.out.println("****** BUSCAR A UN ESTUDIANTE ******");
+                    System.out.println("PUEDE BUSCAR POR NOMBRE O CÓDIGO. ELEGIR UNA OPCIÓN:");
+                    System.out.println("A – APELLIDO");
+                    System.out.println("B – CÓDIGO");
+                    System.out.print("ELEGIR UNA OPCIÓN: ");
+                    char opcionBusqueda = scanner.next().charAt(0);
+
+                    if (opcionBusqueda == 'A') {
+                        System.out.print("INGRESE EL APELLIDO DEL ALUMNO: ");
+                        String apellidoBusqueda = scanner.next();
+
+                        // Buscar estudiantes por apellido
+                        boolean encontrado = false;
+                        for (Estudiante estudiante : listaEstudiantes.getEstudiantes()) {
+                            if (estudiante.getNombre().toLowerCase().contains(apellidoBusqueda.toLowerCase())) {
+                                System.out.println(estudiante.toString());
+                                encontrado = true;
+                            }
+                        }
+                        if (!encontrado) {
+                            System.out.println("(*) Si no existe indicar el alumno no está registrado.");
+                        }
+                    } else if (opcionBusqueda == 'B') {
+                        System.out.print("INGRESE EL CÓDIGO DEL ALUMNO: ");
+                        int codigoBusqueda = scanner.nextInt();
+
+                        // Buscar estudiantes por código
+                        boolean encontrado = false;
+                        for (Estudiante estudiante : listaEstudiantes.getEstudiantes()) {
+                            if (estudiante.getCodigo() == codigoBusqueda) {
+                                System.out.println(estudiante.toString());
+                                encontrado = true;
+                                break; // Puedes detener la búsqueda una vez que encuentres el estudiante.
+                            }
+                        }
+                        if (!encontrado) {
+                            System.out.println("(*) Si no existe indicar el alumno no está registrado.");
+                        }
+                    } else {
+                        System.out.println("Opción no válida.");
+                    }
+
+                    break;
+                
