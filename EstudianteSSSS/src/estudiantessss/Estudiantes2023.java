be4f721607971ffde69daa987d5616a80f4767a1
DieselAponte <143888820+DieselAponte@users.noreply.github.com>
2023-10-30 23:32:08 -0500

    Diesel Aponte

--- EstudianteSSSS/src/estudiantessss/Estudiantes2023.java
+++ EstudianteSSSS/src/estudiantessss/Estudiantes2023.java
@@ -5,6 +5,7 @@ import javax.swing.*;
 import java.io.*;
 
 public class Estudiantes2023 {
+
     private static ArrayList<Estudiante> estudiantes;
 
     public Estudiantes2023() {
@@ -14,31 +15,54 @@ public class Estudiantes2023 {
 
     public void adicionar(Estudiante estudiante) {
         estudiantes.add(estudiante);
+        try {
+            FileWriter writer = new FileWriter("estudiantes.txt", true);
+            writer.write(estudiante.getCodigo() + "," + estudiante.getNombre() + "," + estudiante.getCiclo() + "," + estudiante.getPension() + "\n");
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Error al escribir en el archivo estudiantes.txt.");
+        }
     }
 
     public void eliminar(Estudiante estudiante) {
         //Introduce codigo
     }
 
+    public ArrayList<Estudiante> getEstudiantes() {
+        return estudiantes;
+    }
+
+    public void actualizarArchivo() {
+        try {
+            FileWriter writer = new FileWriter("estudiantes.txt");
+            for (Estudiante estudiante : estudiantes) {
+                writer.write(estudiante.getCodigo() + "," + estudiante.getNombre() + "," + estudiante.getCiclo() + "," + estudiante.getPension() + "\n");
+            }
+            writer.close();
+        } catch (IOException e) {
+            System.out.println("Error al actualizar el archivo estudiantes.txt.");
+        }
+    }
+
     public Estudiante obtener(int pos) {
-	//Introduce codigo
+        //Introduce codigo
         return null;
     }
 
     public Estudiante buscar(int codigo) {
-	//Introduce codigo        
-	  return null;
+        //Introduce codigo        
+        return null;
     }
 
     public int tamano() {
         //Introduce codigo        
-	  return 0;
+        return 0;
     }
 
-    public void listarEstudiantes(){
-        for(Estudiante estudiante: estudiantes){
+    public void listarEstudiantes() {
+        for (Estudiante estudiante : estudiantes) {
             System.out.println(estudiante.toString());
-        }    
+        }
     }
 
     // M?todos para manipular el archivo de texto
@@ -52,8 +76,8 @@ public class Estudiantes2023 {
         br.close();
     }
 
-	// Metodo que añadir a los estudiantes del archivo .txt en el arrayList estudiantes.
-    private void createStudent(StringTokenizer st){
+    // Metodo que añadir a los estudiantes del archivo .txt en el arrayList estudiantes.
+    private void createStudent(StringTokenizer st) {
         int codigo = Integer.parseInt(st.nextToken().trim());
         String nombre = st.nextToken().trim();
         int ciclo = Integer.parseInt(st.nextToken().trim());
@@ -70,9 +94,10 @@ public class Estudiantes2023 {
             InputStream inputStream = clazz.getResourceAsStream("/EstudianteSSSS/estudiantes.txt");
             if (file.exists()) {
                 readFromInputStream(inputStream);
-            } else
+            } else {
                 JOptionPane.showMessageDialog(null,
                         "El archivo estudiantes.txt no existe");
+            }
         } catch (Exception x) {
             JOptionPane.showMessageDialog(null, "Se produjo un error= " + x);
         }
